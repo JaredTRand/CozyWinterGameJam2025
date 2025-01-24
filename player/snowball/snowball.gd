cce95b478f43ec039018f4e1f2aefc4e3e67107f
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2025-01-24 00:46:20 -0800

    sound

--- player/snowball/snowball.gd
+++ player/snowball/snowball.gd
@@ -2,12 +2,15 @@ extends RigidBody3D
 @export var speed:int = 100
 @onready var snow_break:GPUParticles3D = $snow_break
 
-func _on_body_entered(body: Node) -> void:
-	print("aaaaa")
-	$CollisionShape3D.disabled = true
-	$MeshInstance3D.visible = false
-	snow_break.restart()
+var has_hit = false
 
+func _on_body_entered(body: Node) -> void:
+	if not has_hit:
+		has_hit = true
+		$CollisionShape3D.disabled = true
+		$MeshInstance3D.visible = false
+		snow_break.restart()
+		$AudioStreamPlayer3D.play()
 
 func _on_snow_break_finished() -> void:
 	queue_free()
